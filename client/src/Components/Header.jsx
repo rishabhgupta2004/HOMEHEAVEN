82a312a57cab6476dbc1b196bdbbe1620cb31624
RISHABH GUPTA <rishabh78000gupta@gmail.com>
2024-06-27 19:17:14 +0530

    header page functionality

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
 
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
-    <header>
-      <h1 className='font-bold text-sm sm:text-xl'>
-        <span className='text-pink-400'>Home</span>
-        <span className='text-green-700'>Heaven</span>
+    <header className='bg-blue-200 shadow-md'>
+      <div className='flex justify-between item-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+      <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+        <span className='text-pink-500'>Home</span>
+        <span className='text-blue-700'>Heaven</span>
       </h1>
+      </Link>
+      <form className='bg-slate-100 p-3 rounded-lg flex item-center'>
+        <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'>
+        
+        </input>
+        <FaSearch className="text-slate-600"/>
+        
+      </form>
+      <ul className="flex gap-4">
+        
+        <Link to='/'>
+        <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
+        </Link>
+        <Link to='/about'>
+      
+        <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
+        </Link>
+        <Link to='/sign-in'>
+        <li className=" text-slate-700 hover:underline">{' '}SignIn</li>
+        </Link>
+      </ul>
+
+      </div>
     </header>
   )
 }
