672e85c6e5031448c55e0ac3312c5d80152fbacc
RISHABH GUPTA <rishabh78000gupta@gmail.com>
2024-06-27 22:38:06 +0530

    privateroute

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,7 +1,9 @@
 
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 export default function Header() {
+  const {currentUser}=useSelector(state=>state.user)
   return (
     <header className='bg-blue-200 shadow-md'>
       <div className='flex justify-between item-center max-w-6xl mx-auto p-3'>
@@ -27,9 +29,17 @@ export default function Header() {
       
         <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
         </Link>
-        <Link to='/sign-in'>
-        <li className=" text-slate-700 hover:underline">{' '}SignIn</li>
-        </Link>
+        <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
+          </Link>
       </ul>
 
       </div>
