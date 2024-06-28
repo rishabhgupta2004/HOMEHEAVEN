83a8bf253858f4d501a59bf2f51fe8fcf86c56ae
RISHABH GUPTA <rishabh78000gupta@gmail.com>
2024-06-28 12:26:42 +0530

    auth controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -66,3 +66,11 @@ export const signin = async (req, res, next) => {
       next(error);
     }
   };
+  export const signOut = async (req, res, next) => {
+    try {
+      res.clearCookie('access_token');
+      res.status(200).json('User has been logged out!');
+    } catch (error) {
+      next(error);
+    }
+  };
