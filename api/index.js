6f6635212df55377a1ed0bab8ab35e6df05d2c9c
RISHABH GUPTA <rishabh78000gupta@gmail.com>
2024-06-28 18:51:06 +0530

    completed

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -15,7 +16,7 @@ app.use(express.json())
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
 })
-
+app.use(cookieParser())
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 
